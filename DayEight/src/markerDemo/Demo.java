cb922023cda9c6baa50bd7e499ce26bbdb0808a9
Geetanjali Bejjalwar <bejjalwargeetanjali@gmail.com>
2025-07-09 10:53:32 +0530

    .

--- DayEight/src/markerDemo/Demo.java
+++ DayEight/src/markerDemo/Demo.java
@@ -1,5 +1,18 @@
 package markerDemo;
 
-public class Demo {
+public class Demo implements ExtendedInterface	 {
+
+	@Override
+	public void show() {
+		System.out.println("hello");
+		
+	}
+
+	@Override
+	public void print() {
+		System.out.println("Everyone");
+		
+	}
+	 
 
 }
