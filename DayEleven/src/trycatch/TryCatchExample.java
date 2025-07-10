bc16dd28f19299b56c2cb0a0dd31699a402dbdf8
Geetanjali Bejjalwar <bejjalwargeetanjali@gmail.com>
2025-07-10 10:07:40 +0530

    .

---
+++ DayEleven/src/trycatch/TryCatchExample.java
@@ -0,0 +1,23 @@
+package trycatch;
+
+public class TryCatchExample {
+	
+	static void performDivision(int x,int y) {
+		
+		System.out.println("I am in method");
+		int z;
+		try
+		{
+			System.out.println("I am in try block");
+		
+		z = x/y;
+		System.out.println("Result of z" +z);
+		
+	}
+	catch ( ArithmeticException e )
+	{
+		System.err.println(e);
+	}
+
+	}
+}
