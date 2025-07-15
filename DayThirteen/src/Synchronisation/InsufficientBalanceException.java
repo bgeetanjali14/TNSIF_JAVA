4c7d9127a3ba21e1bdff128eb111a70621584e31
Geetanjali Bejjalwar <bejjalwargeetanjali@gmail.com>
2025-07-15 07:03:37 +0530

    .

---
+++ DayThirteen/src/Synchronisation/InsufficientBalanceException.java
@@ -0,0 +1,14 @@
+package Synchronisation;
+
+public class InsufficientBalanceException extends Exception {
+
+	public InsufficientBalanceException()
+	{
+		super("Insufficient balance in your account");
+	}
+	
+	public InsufficientBalanceException(String message)
+	{
+		super(message);
+	}
+}
